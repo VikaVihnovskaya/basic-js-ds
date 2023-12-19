e15a5cca35085819aaf5ed4959c448c3d6509214
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2023-12-19 22:50:15 +0300

    feat: solve task for stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    arr = [];
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    push(element) {
+        this.arr.push(element);
+    }
+
+    pop() {
+        return this.arr.pop();
+    }
+
+    peek() {
+        return this.arr[this.arr.length - 1];
+    }
 }
 
 module.exports = {
-  Stack
+    Stack
 };
